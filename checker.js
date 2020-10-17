8b8ea5c0e0c840eee1bd8dd2305520d11f52e426
qolbudr <qolbu.dzikru30@gmail.com>
2020-10-17 01:47:11 -0700

    change works bin api

--- checker.js
+++ checker.js
@@ -5,13 +5,12 @@ const request = require('request');
 
 
 const config = require('./config.json');
-const key = config.api_key;
 const token = config.token;
 const prefix = config.prefix;
 
 client.on("ready", function() {
     console.log("[".white + "+".cyan + "]".white + " -".cyan + " BinChecker V1.0 crée et développé par".white + " Elohîm Siraak_#1607".cyan + ".".white);
-    console.log("[".white + "+".cyan + "]".white + " -".cyan + " Le checker utilise une api bincode (".white + "https://bincodes.com/".cyan + ")".white + ".".white);
+    console.log("[".white + "+".cyan + "]".white + " -".cyan + " Le checker utilise une api bincode (".white + "https://binlist.net/".cyan + ")".white + ".".white);
 });
 
 client.on("message", function(m) {
@@ -27,10 +26,10 @@ client.on("message", function(m) {
                 return;
             } else {
                 if(m.deletable) m.delete()
-                request(`http://api.bincodes.com/bin/?format=json&api_key=${key}&bin=${args[1]}`, function(error, response, body) {
+                request(`https://lookup.binlist.net/${args[1]}`, function(error, response, body) {
                     if(!error && response.statusCode === 200) {
                         let page = JSON.parse(body);
-                        m.channel.send("> BIN : " + args[1] + "\n> Banque : " + page.bank + "\n> Marque : " + page.card + "\n> Type : " + page.type + "\n> Niveau : " + page.level + "\n> Pays : " + page.country)
+                        m.channel.send("> BIN : " + args[1] + "\n> Banque : " + page.bank.name + "\n> Marque : " + page.brand + "\n> Type : " + page.type + "\n> Pays : " + page.country.name)
                     }
                 })
             }
